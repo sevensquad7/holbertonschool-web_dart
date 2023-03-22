3a60c9e6cef084242e14161050acd444d606a834
sevensquad7 <edgarxot@gmail.com>
2023-03-21 20:12:16 -0500

    load change in code

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,47 +1,20 @@
 import 'dart:convert';
-import 'dart:core';
+import '4-util.dart';
 
-Future<String> fetchUserOrders(String id) async {
-  var orders = {
-    "7ee9a243-01ca-47c9-aa14-0149789764c3": ["pizza", "orange"]
-  };
+Future<dynamic> calculateTotal() async {
   try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(orders[id]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-Future<String> fetchProductPrice(product) async {
-  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
-  try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(products[product]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-calculateTotal() async {
-    try {
-        double price = 0;
-	
-        final Map<String, dynamic> userData = json.decode(await fetchUserData());
-        final String data = userData['id'];
-        final List<dynamic> userOrder = json.decode(await fetchUserOrders(data));
-        for (int idx = 0; idx < userOrder.length; idx++) {
-            price += json.decode(await fetchProductPrice(userOrder[idx]));
-        }
-        return price;
-    } catch (err) {
-        print('error caught: $err');
-        return -1;
+    var userData = await fetchUserData();
+    var jsonUser = jsonDecode(userData);
+    var userOrder = await fetchUserOrders(jsonUser["id"]);
+    List orderList = jsonDecode(userOrder);
+    var orderPrice;
+    var price = 0.0;
+    for (var i = 0; i < orderList.length; i++) {
+      orderPrice = await fetchProductPrice(orderList[i]);
+      price += num.parse(orderPrice);
     }
+    return price;
+  } catch (err) {
+    return -1;
+  }
 }
